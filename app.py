0af7da7fe46e5e4c996871d26c01a3dd2e8c727e
toufanialfarisi <toufani.deeplearning@gmail.com>
2021-01-06 11:32:41 +0700

    add progresss data model

--- app.py
+++ app.py
@@ -30,7 +30,118 @@ class ModelPekerjaan(db.Model):
     updated = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
 
+class ModelProgressPekerjaan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    n_spjl_begih = db.Column(db.Integer)
+    n_spkpj_begih = db.Column(db.Integer)
+    n_spbl_begih = db.Column(db.Integer)
+    n_spkpb_begih = db.Column(db.Integer)
+    n_spjl_betrak = db.Column(db.Integer)
+    n_spkpj_betrak = db.Column(db.Integer)
+    n_spbl_betrak = db.Column(db.Integer)
+    n_spkpb_betrak = db.Column(db.Integer)
+    val_spjl_begih = db.Column(db.Float)
+    val_spkpj_begih = db.Column(db.Float)
+    val_spbl_begih = db.Column(db.Float)
+    val_spkpb_begih = db.Column(db.Float)
+    val_spjl_betrak = db.Column(db.Float)
+    val_spkpj_betrak = db.Column(db.Float)
+    val_spbl_betrak = db.Column(db.Float)
+    val_spkpb_betrak = db.Column(db.Float)
+
+
 db.create_all()
+'''
+http://localhost:5000/siap-bayar/api/v1/progress?n_spjl_begih=1&n_spkpj_begih=1&n_spbl_begih=1&n_spkpb_begih=1&n_spjl_betrak=1&n_spkpj_betrak=1&n_spbl_betrak=1&n_spkpb_betrak=1&val_spjl_begih=1&val_spkpj_begih=1&val_spbl_begih=1&val_spkpb_begih=1&val_spjl_betrak=1&val_spkpj_betrak=1&val_spbl_betrak=1&val_spkpb_betrak=1
+'''
+
+
+class GetProgressData(Resource):
+    def get(self):
+        n_spjl_begih = request.args.get('n_spjl_begih')
+        n_spkpj_begih = request.args.get('n_spkpj_begih')
+        n_spbl_begih = request.args.get('n_spbl_begih')
+        n_spkpb_begih = request.args.get('n_spkpb_begih')
+        n_spjl_betrak = request.args.get('n_spjl_betrak')
+        n_spkpj_betrak = request.args.get('n_spkpj_betrak')
+        n_spbl_betrak = request.args.get('n_spbl_betrak')
+        n_spkpb_betrak = request.args.get('n_spkpb_betrak')
+        val_spjl_begih = request.args.get('val_spjl_begih')
+        val_spkpj_begih = request.args.get('val_spkpj_begih')
+        val_spbl_begih = request.args.get('val_spbl_begih')
+        val_spkpb_begih = request.args.get('val_spkpb_begih')
+        val_spjl_betrak = request.args.get('val_spjl_betrak')
+        val_spkpj_betrak = request.args.get('val_spkpj_betrak')
+        val_spbl_betrak = request.args.get('val_spbl_betrak')
+        val_spkpb_betrak = request.args.get('val_spkpb_betrak')
+        try:
+            query = ModelProgressPekerjaan.query.all()[0]
+            query.n_spjl_begih = request.args.get("n_spjl_begih")
+            query.n_spkpj_begih = request.args.get("n_spkpj_begih")
+            query.n_spbl_begih = request.args.get("n_spbl_begih")
+            query.n_spkpb_begih = request.args.get("n_spkpb_begih")
+            query.n_spjl_betrak = request.args.get("n_spjl_betrak")
+            query.n_spkpj_betrak = request.args.get("n_spkpj_betrak")
+            query.n_spbl_betrak = request.args.get("n_spbl_betrak")
+            query.n_spkpb_betrak = request.args.get("n_spkpb_betrak")
+            query.val_spjl_begih = request.args.get("val_spjl_begih")
+            query.val_spkpj_begih = request.args.get("val_spkpj_begih")
+            query.val_spbl_begih = request.args.get("val_spbl_begih")
+            query.val_spkpb_begih = request.args.get("val_spkpb_begih")
+            query.val_spjl_betrak = request.args.get("val_spjl_betrak")
+            query.val_spkpj_betrak = request.args.get("val_spkpj_betrak")
+            query.val_spbl_betrak = request.args.get("val_spbl_betrak")
+            query.val_spkpb_betrak = request.args.get("val_spkpb_betrak")
+            db.session.commit()
+            response = {
+                "n_spjl_begih": query.n_spjl_begih,
+                "n_spkpj_begih": query.n_spkpj_begih,
+                "n_spbl_begih": query.n_spbl_begih,
+                "n_spkpb_begih": query.n_spkpb_begih,
+                "n_spjl_betrak": query.n_spjl_betrak,
+                "n_spkpj_betrak": query.n_spkpj_betrak,
+                "n_spbl_betrak": query.n_spbl_betrak,
+                "n_spkpb_betrak": query.n_spkpb_betrak,
+                "val_spjl_begih": query.val_spjl_begih,
+                "val_spkpj_begih": query.val_spkpj_begih,
+                "val_spbl_begih": query.val_spbl_begih,
+                "val_spkpb_begih": query.val_spkpb_begih,
+                "val_spjl_betrak": query.val_spjl_betrak,
+                "val_spkpj_betrak": query.val_spkpj_betrak,
+                "val_spbl_betrak": query.val_spbl_betrak,
+                "val_spkpb_betrak": query.val_spkpb_betrak,
+                "n_begih": query.n_spjl_begih + query.n_spkpj_begih + query.n_spbl_begih + query.n_spkpb_begih,
+                "n_betrak": query.n_spjl_betrak + query.n_spkpj_betrak + query.n_spbl_betrak + query.n_spkpb_betrak,
+                "val_begih": query.val_spjl_begih + query.val_spkpj_begih + query.val_spbl_begih + query.val_spkpb_begih,
+                "val_betrak": query.val_spjl_betrak + query.val_spkpj_betrak + query.val_spbl_betrak + query.val_spkpb_betrak,
+            }
+            return response, 200
+        except:
+            data = ModelProgressPekerjaan(
+                n_spjl_begih=n_spjl_begih,
+                n_spkpj_begih=n_spkpj_begih,
+                n_spbl_begih=n_spbl_begih,
+                n_spkpb_begih=n_spkpb_begih,
+                n_spjl_betrak=n_spjl_betrak,
+                n_spkpj_betrak=n_spkpj_betrak,
+                n_spbl_betrak=n_spbl_betrak,
+                n_spkpb_betrak=n_spkpb_betrak,
+                val_spjl_begih=val_spjl_begih,
+                val_spkpj_begih=val_spkpj_begih,
+                val_spbl_begih=val_spbl_begih,
+                val_spkpb_begih=val_spkpb_begih,
+                val_spjl_betrak=val_spjl_betrak,
+                val_spkpj_betrak=val_spkpj_betrak,
+                val_spbl_betrak=val_spbl_betrak,
+                val_spkpb_betrak=val_spkpb_betrak
+            )
+            db.session.add(data)
+            db.session.commit()
+            response = {
+                "message": "sukses input data",
+                "code": 200
+            }
+            return response, 200
 
 
 class GetAllData(Resource):
@@ -144,6 +255,8 @@ class InserData(Resource):
 
 api.add_resource(GetAllData, "/siap-bayar/api/v1", methods=["GET", "DELETE"])
 api.add_resource(InserData, "/siap-bayar/api/v1/data", methods=["GET"])
+api.add_resource(
+    GetProgressData, "/siap-bayar/api/v1/progress", methods=["GET"])
 
 if __name__ == "__main__":
     app.run(debug=True)
